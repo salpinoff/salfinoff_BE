143627e0ec0dfbb9662182e46b5e8c7db555dbe5
62hoon99 <ske06008@gmail.com>
2024-04-13 22:08:39 +0900

    init

---
+++ src/main/java/com/server/salpinoffServer/SalpinoffServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.salpinoffServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalpinoffServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalpinoffServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/salpinoffServer/SalpinoffServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.salpinoffServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalpinoffServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
