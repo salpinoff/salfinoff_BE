3f41e50caab8c8eaf158c5cdbd2a9e8f381ab003
62hoon99 <ske06008@gmail.com>
2024-05-15 21:53:35 +0900

    chore: localhost 3000 port 만 cors 해제

--- src/main/java/com/server/salpinoffServer/infra/auth/config/SecurityConfig.java
+++ src/main/java/com/server/salpinoffServer/infra/auth/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.setAllowCredentials(true);
         configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
