4f155cabf7687050014618357153fc77e08b5d0c
62hoon99 <ske06008@gmail.com>
2024-07-21 11:01:25 +0900

    update: api 호출 로깅은 스테이징 환경에서만 작동하도록 수정

--- src/main/java/com/server/salpinoffServer/infra/logging/LoggingAspect.java
+++ src/main/java/com/server/salpinoffServer/infra/logging/LoggingAspect.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -13,6 +14,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Aspect
 @Component
 @Slf4j
+@Profile("stage")
 public class LoggingAspect {
 
     @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
